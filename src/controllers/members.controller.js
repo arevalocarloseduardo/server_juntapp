6c5cd875df3e25c37fa4305459e827fda9dde749
carevalo <carevalo@openpass.com.ar>
2023-03-03 18:21:26 -0300

    merge

--- src/controllers/members.controller.js
+++ src/controllers/members.controller.js
@@ -4,9 +4,9 @@ import { pool } from '../db.js'
 export const getMembers = async (req, res) => {
     try {
 
-        const [rows] = await pool.query('SELECT * FROM members',)
+        const ownerId = [rows] = await pool.query('SELECT * FROM members',)
 
-        res.json(rows)
+        res.json(ownerId)
     } catch (error) {
 
         res.status(500).json({ error: error.message });
@@ -35,10 +35,10 @@ export const deleteMember = async (req, res) => {
 }
 export const createMember = async (req, res) => {
     try {
-        const { first_name, email, memberId} = req.body
+        const { first_name, email, memberId } = req.body
         const [rows] = await pool.query('INSERT INTO members (first_name, last_name, email, ranking, role, date_of_birth, identification_number, address, phone_number, memberId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
             [req.body.first_name, req.body.last_name, req.body.email, req.body.ranking, req.body.role, req.body.date_of_birth, req.body.identification_number, req.body.address, req.body.phone_number, req.body.memberId])
-        res.send({ id: rows.insertId, first_name, email, memberId})
+        res.send({ id: rows.insertId, first_name, email, memberId })
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
